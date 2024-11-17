a267d1cfac9ad26560475f2901254c8da9b3fd83
dlpduy <dinhlephucduy@gmail.com>
2024-11-17 21:48:57 +0700

    create architecture spring boot

---
+++ Backend/src/main/java/com/project/SSPS/SspsApplication.java
@@ -0,0 +1,13 @@
+package com.project.SSPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/SSPS/SspsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SSPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
