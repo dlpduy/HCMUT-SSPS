08ce7029b35e37b54f782cd35c26ceaa98a73718
Phuc Tran Hong <tranhongphucfb@gmail.com>
2024-11-25 18:17:06 +0700

    add payment

---
+++ Backend/src/main/java/com/project/SSPS/dto/PaymentDTO.java
@@ -0,0 +1,14 @@
+package com.project.SSPS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+public abstract class PaymentDTO {
+    @Builder
+    @AllArgsConstructor
+    public static class VNPayResponse {
+        public String code;
+        public String message;
+        public String paymentUrl;
+    }
+}
