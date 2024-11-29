3218000ca5901b4130685d5686fb25b401a9d7f8
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2024-11-29 16:47:12 +0700

    Complete Statistic

--- Frontend/src/page/private/spso/Dashboard/index.jsx
+++ Frontend/src/page/private/spso/Dashboard/index.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { getStatistic } from "../../../../api/spso";
+import { Statistic } from "antd";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [statistic, setStatistic] = useState();
+
+  useEffect(() => {
+    getStatistic().then((res) => setStatistic(res.data));
+    const interval = setInterval(() => {
+      getStatistic().then((res) => setStatistic(res.data));
+    }, 10 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="grid grid-cols-2 grid-rows-1 gap-4 w-1/2 h-1/2 bg-white rounded-3xl border border-darkblue">
+        <Statistic title="Tổng số dịch vụ in đã thực hiện" value={statistic?.total_printing} className="place-content-center place-self-center" />
+        <Statistic title="Tổng số giao dịch đã thực hiện" value={statistic?.total_printing} className="place-content-center place-self-center" />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
